a44e8d09049fac85dabe91a3a91960d3f264bf3a
bigcanbigbig <jupiter2432321@gmail.com>
2016-04-14 12:27:04 +0800

    Added files via upload

--- A/profile.php
+++ A/profile.php
@@ -6,12 +6,14 @@
 	$readresult=mysqli_query($link,$read);
 	$result=mysqli_fetch_array($readresult);
 
-	echo "<form action='regresult.php' method='post'>";
-	echo "帳號：<input type='text' name='Account'><br/><br/>";
-	echo "密碼：密碼：<input type='text' name='Pwd'><br/><br/>";
-	echo "名稱：<input type='text' name='Name'><br/><br/>";
-	echo 'Email：<input type="text" name="Email"><br/><br/>';
-	echo '電話：<input type="text" name="Phone"><br/><br/>';
+	echo "<form action='updateresult.php' method='post'>";
+
+	echo "<input type='hidden' name='no' id='no' value=".$result[0].">";
+	echo "帳號：<input type='text' name='Account' value=".$result[1]."><br/><br/>";
+	echo "密碼：密碼：<input type='text' name='Pwd' value=".$result[2]."><br/><br/>";
+	echo "名稱：<input type='text' name='Name' value=".$result[3]."><br/><br/>";
+	echo "Email：<input type='text' name='Email' value=".$result[4]."><br/><br/>";
+	echo "電話：<input type='text' name='Phone' value=".$result[5]."><br/><br/>";
 	echo '<input type="submit" value="修改">
 			<input type="reset" value="重填">';
 ?>
