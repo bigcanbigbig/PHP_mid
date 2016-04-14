b759ac4920844e254ede222226f1fa30a5807236
bigcanbigbig <jupiter2432321@gmail.com>
2016-04-14 12:30:08 +0800

    Added files via upload

---
+++ A/updateresult.php
@@ -0,0 +1,16 @@
+<?php
+
+	$no=$_POST["no"];
+	$Account=$_POST["Account"];
+	$Pwd=$_POST["Pwd"];
+	$Name=$_POST["Name"];
+	$Email=$_POST["Email"];
+	$Phone=$_POST["Phone"];
+
+	include("../include.php");
+
+	$update="UPDATE user SET Account='$Account',Pwd='Pwd',Name='$Name',Email='$Email',Phone='$Phone' WHERE no=".$no;
+
+	mysqli_query($link,$update);
+	echo "更新成功";
+?>
