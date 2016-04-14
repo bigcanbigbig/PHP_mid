a28c2669ac9a953c65535f6de26110bba04af7bc
bigcanbigbig <jupiter2432321@gmail.com>
2016-04-14 12:20:39 +0800

    Added files via upload

--- A/profile.php
+++ A/profile.php
@@ -2,9 +2,10 @@
 	$no=$_GET["no"];
 	include("include.php");
 
-	$read="SELECT * FROM record WHERE no=".$no;
+	$read="SELECT * FROM user WHERE no=".$no;
 	$readresult=mysqli_query($link,$read);
 	$result=mysqli_fetch_array($readresult);
+
 	echo "<form action='regresult.php' method='post'>";
 	echo "帳號：<input type='text' name='Account'><br/><br/>";
 	echo "密碼：密碼：<input type='text' name='Pwd'><br/><br/>";
