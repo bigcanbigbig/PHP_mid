e54b0218af405262cf2d6f54686e02decbb9793c
bigcanbigbig <jupiter2432321@gmail.com>
2016-04-14 12:18:42 +0800

    Added files via upload

--- A/profile.php
+++ A/profile.php
@@ -6,4 +6,11 @@
 	$readresult=mysqli_query($link,$read);
 	$result=mysqli_fetch_array($readresult);
 	echo "<form action='regresult.php' method='post'>";
+	echo "帳號：<input type='text' name='Account'><br/><br/>";
+	echo "密碼：密碼：<input type='text' name='Pwd'><br/><br/>";
+	echo "名稱：<input type='text' name='Name'><br/><br/>";
+	echo 'Email：<input type="text" name="Email"><br/><br/>';
+	echo '電話：<input type="text" name="Phone"><br/><br/>';
+	echo '<input type="submit" value="註冊">
+			<input type="reset" value="重填">';
 ?>
